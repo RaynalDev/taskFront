389698525939252b803777425e8ac035208f4e12
Gold Smith <gholdsmith@gmail.com>
2024-06-21 15:36:31 +0200

    log url

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 let express = require('express');
 let app = express();
+const PORT = process.env.PORT || 4200;
+const URL = process.env.URL;
 app.use(express.static(__dirname + '/dist/task-front/browser'));
 app.get('/*', (req, res)=>{
-    console.log("GET From SERVER");
-    console.log('process.env.PORT : ' + process.env.PORT);
     res.sendFile(__dirname + '/dist/task-front/browser/index.html');
 })
 
-app.listen(process.env.PORT || 4200);
+app.listen(PORT, ()=>{
+    console.log('Server is running on port ' + PORT);
+    console.log('Server is running on URL ' + URL);
+});
