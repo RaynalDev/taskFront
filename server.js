85dd7d96947c0364b6b5b3e15fc45930219f0a96
Gold Smith <gholdsmith@gmail.com>
2024-06-21 15:03:59 +0200

    config railway

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ let express = require('express');
 let app = express();
 app.use(express.static(__dirname + '/dist/task-front/browser'));
 app.get('/*', (req, res)=>{
+    console.log("GET From SERVER");
+    console.log('process.env.PORT : ' + process.env.PORT);
     res.sendFile(__dirname + '/dist/task-front/browser/index.html');
 })
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 4200);
