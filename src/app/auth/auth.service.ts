b2efb36c194604b6c757da4b8ab76f5cac4a9fe7
Gold Smith <gholdsmith@gmail.com>
2024-07-22 14:07:49 +0200

    authService created

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,17 +2,29 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-  private readonly apiurl = 'http://localhost:3000';
+  private readonly apiUrl = environment.apiUrl;
   private readonly tokenName = 'auth_token';
 
-  constructor(private http: HttpClient, private router:Router) { }
-  
-  login(email: string, password: string){
-    
+  constructor(private http: HttpClient, private router: Router) {}
+
+  login(email: string, password: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/login`, { email, password });
+  }
+
+  logout(){
+    localStorage.removeItem(this.tokenName);
+    this.router.navigate(['/login']);
+  }
+
+  isAuthenticated(): boolean {
+    const token = localStorage.getItem(this.tokenName);
+
+    return !!token;
   }
 }
